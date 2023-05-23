d67b2fca8b6fd29fbf177ee62c8e08d74fcac4dd
Nursultan Khalilov <nursultan.xalilov@kapitalbank.az>
2023-05-23 23:59:28 +0400

    create getStudentById service

--- src/main/java/com/university/register/universityregister/repository/StudentRepository.java
+++ src/main/java/com/university/register/universityregister/repository/StudentRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student, Integer> {
     List<Student> findStudentByGroupNo(String grupNo);
     List<Student> findStudentByProfession(String profession);
-
 }
